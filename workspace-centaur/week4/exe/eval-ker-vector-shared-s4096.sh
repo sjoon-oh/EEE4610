7a3aa280f71ec7ffab5f1e9ea20dd1a8d61031d0
acoustikue <acoustikue@gmail.com>
2021-01-31 21:05:38 +0900

    mod

---
+++ workspace-centaur/week4/exe/eval-ker-vector-shared-s4096.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+filelist=`ls ./dataset/s4096*.mtx`
+savefile=""
+wait
+
+#./spmv-cusparse 1 dataset/s32-r10.000000-mat.mtx | grep "Average" | sed -E 's/[^0-9. ]*//g'
+for filename in $filelist
+do
+	for i in {1..50}
+	do
+		echo "Running for $filename, for iteration $i"
+
+		savefile=`echo $filename | sed -E 's/.mtx//g' | sed -E 's/.\/dataset\///g'`
+		echo "    Setting file name to $savefile.csv"
+
+		./spmv-ker-vector-shared 1 $filename | grep "Average" | sed -E 's/[^0-9. ]*//g' | sed 's/ //g' >> "./csv/ker_vector_shared/$savefile.csv"
+		wait $!
+	done
+done
