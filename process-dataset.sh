ca580cc6e90165fdc9eae54acceb8257fa488857
sjoon-oh <sjoon.oh@protonmail.ch>
2021-03-26 11:27:31 +0900

    update

--- process-dataset.sh
+++ process-dataset.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-python3 ./process-amazon-dataset.py \
---dataset "reviews_Electronics_5.json" \
---dataset-rm-key reviewerName reviewText summary \
---metadata "meta_Electronics.json" \
---metadata-rm-key description imUrl
+python3 ./analyze-ard.py \
+--filename "reviews_Electronics_5.json" \
+--rm-key reviewerName reviewText summary reviewerID helpful 
---
+++ process-metadata.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python3 ./analyze-ard.py \
+--filename "meta_Electronics.json" \
+--rm-key description imUrl title price
+# --metadata "meta_Electronics.json" \
+# --metadata-rm-key description imUrlmetadata-rm-ke
