7430b89b4d6275aef66d685c13af0334acc31200
sjoon-oh <sjoon.oh@protonmail.ch>
2021-03-27 00:07:30 +0900

    error fix:

--- dataset/process.sh
+++ dataset/process.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-json_files=`ls ./*.txt`
+json_files=`ls ./*.json`
 
-for eachfile in $json_files
+for eachfile in $json_files; do
     echo "Running script for $eachfile"
     python3 analyze-ard.py --filename $eachfile \
         --rm-key reviewerName reviewText summary reviewerID helpful image
