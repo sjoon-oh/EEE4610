bf32ec33f345b6d6d716716dcd9c680feb0a9e23
sjoon-oh <sjoon.oh@protonmail.ch>
2021-03-28 08:19:58 +0900

    update

--- process.sh
+++ process.sh
@@ -5,5 +5,5 @@ json_files=`ls ./*.json`
 for eachfile in $json_files; do
     echo "Running script for $eachfile"
     python3 analyze-ard.py --filename $eachfile \
-        --rm-key reviewerName reviewText summary reviewerID helpful image description tech1 tech2 similar_item price title feature also_view main_cat rank details
+        --rm-key reviewerName reviewText summary reviewerID helpful image description tech1 tech2 similar_items price
 done
