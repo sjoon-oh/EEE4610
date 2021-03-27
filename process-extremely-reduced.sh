bddb4af8d81e70c5d710aa5a091ec75523dd160b
sjoon-oh <sjoon.oh@protonmail.ch>
2021-03-28 06:28:36 +0900

    update

--- process-extremely-reduced.sh
+++ process-extremely-reduced.sh
@@ -5,5 +5,5 @@ json_files=`ls ./*.json`
 for eachfile in $json_files; do
     echo "Running script for $eachfile"
     python3 analyze-ard.py --filename $eachfile \
-        --rm-key reviewerName reviewText summary reviewerID helpful image description tech1 tech2 similar_item price title feature also_view main_cat rank also_buy fit asin
+        --rm-key reviewerName reviewText summary reviewerID helpful image description tech1 tech2 similar_item price title feature also_view main_cat rank also_buy fit overall verified
 done
